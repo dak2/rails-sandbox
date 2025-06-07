c3128acdd27561fc9aca3ca4f02f0a658d15fb56
dak2 <dawt2h@gmail.com>
2025-06-07 23:12:18 +0900

    annotate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  content    :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Post < ApplicationRecord
   validates :content, presence: true, length: { maximum: 280 }
 end
